32d9e8c2d78db410d0479827d421eea5205f7444
Udutha <Rohith.Udutha@team.telstra.com>
2023-08-29 10:33:45 +0530

    eurekaclient

---
+++ src/main/java/com/eureka/Eurekaclientdemo1Application.java
@@ -0,0 +1,13 @@
+package com.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eurekaclientdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eurekaclientdemo1Application.class, args);
+	}
+
+}
